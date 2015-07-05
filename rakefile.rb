1f466d9af8d9ade95d8f4178f3f9047bc05f2dd4
Oskar Gewalli <oskar@gewalli.se>
2015-07-05 10:33:03 +0200

    ant files

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,11 @@
-
-namespace :jisop do
-  task :compile do
-    cd "jisop" do
-      opt = "-quiet"
-      if ENV["windir"] 
-        sh "ant.bat #{opt}"
-      else
-        sh "ant #{opt}"
-      end
+task :default => [:compile]
+task :compile do
+  cd "jisop" do
+    opt = "-quiet -f jisop.xml"
+    if ENV["windir"] 
+      sh "ant.bat #{opt}"
+    else
+      sh "ant #{opt}"
     end
   end
 end
